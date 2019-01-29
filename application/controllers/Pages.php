5e9ec9256821965a5a522e29d648bfc7aa004a02
Feeloh <philipmwendwa3@gmail.com>
2019-01-29 09:16:59 +0300

    initial file required

---
+++ application/controllers/Pages.php
@@ -0,0 +1,20 @@
+<?php
+defined('BASEPATH') OR exit('No direct script access allowed');
+
+class Pages extends CI_Controller
+{
+    public function __contruct()
+    {
+        parent::__construct();
+    }
+
+    public function index(){
+        // echo "I am awesome";
+        $message['khalid'] = 'Keys';
+        $message['key'] ='Oh this is doooope!!!!!!';
+        $this->load->view('Pages/Elements/page_header', $message);
+        $this->load->view('Pages/Elements/navigation');
+        $this->load->view('Pages/home', $message);
+        $this->load->view('Pages/Elements/footer');
+    }
+}
